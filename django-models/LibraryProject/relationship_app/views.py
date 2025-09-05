c8afb7dde1fe1744921922ad4ab76df1e2aded4f
Damen202 <babatundeogundare195@gmail.com>
2025-09-05 23:16:15 +0100

    Django relationship app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView
 from .models import Book, Library
+from .models import Library
 
 
 # Function-based view: List all books
