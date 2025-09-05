b619c1c67cc609c3e7fba784b0118d0d01c9e322
Damen202 <babatundeogundare195@gmail.com>
2025-09-06 00:20:54 +0100

    app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.generic import DetailView
 from .models import Book, Library
@@ -9,7 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 # Function-based view: List all books
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
@@ -18,3 +18,14 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+    # --- Registration view ---
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")  # redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
