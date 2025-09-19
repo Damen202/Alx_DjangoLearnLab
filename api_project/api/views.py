6a145470292841d4ddcaa35b7b3a0626e437f1b7
Damen202 <babatundeogundare195@gmail.com>
2025-09-19 22:38:50 +0100

    router

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,6 +9,6 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewsets(viewsets.ModelViewSet):
+class BookViewset(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
