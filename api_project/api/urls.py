8349a4132d8d026ff8995abe0706347e474ce0b4
Damen202 <babatundeogundare195@gmail.com>
2025-09-19 17:04:33 +0100

    New book and link added

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import BookList
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('books/', BookList.as_view(), name='book_list'),
 
 ]
