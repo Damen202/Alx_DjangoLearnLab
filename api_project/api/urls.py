a592201eabec6e3282b2d19acdf6f6dc89c3ad1e
Damen202 <babatundeogundare195@gmail.com>
2025-09-19 22:35:48 +0100

    router

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import BookList
+from api.views import BookViewsets
+from rest_framework import routers
 
+# Router instance
+router = routers.DefaultRouter()
+router.register(r'books', BookViewsets, basename='book')  # simpler name
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('books/', BookList.as_view(), name='book-list'),
-
+    path('', include(router.urls)),  # includes all CRUD endpoints
 ]
