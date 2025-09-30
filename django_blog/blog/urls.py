b4fc46ea5830de74f7b4f34997dbae7b383444d8
Damen202 <babatundeogundare195@gmail.com>
2025-09-30 14:07:44 +0100

    User

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,12 +23,4 @@ urlpatterns = [
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-
-    # Authentication
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-
-    # Register and profile
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
 ]
