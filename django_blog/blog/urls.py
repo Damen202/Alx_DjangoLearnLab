0c5c6ff8049db31bef076554aee2151303a6d0b3
Damen202 <babatundeogundare195@gmail.com>
2025-09-30 12:51:06 +0100

    User

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,4 +7,13 @@ from django.conf.urls.static import static
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#login and logout
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+#register and profile
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+
+]
