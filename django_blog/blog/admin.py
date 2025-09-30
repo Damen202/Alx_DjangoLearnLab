816f3c63e661740cd31c8ea179757ad195df7c55
Damen202 <babatundeogundare195@gmail.com>
2025-09-30 07:28:12 +0100

    User

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,8 @@
+from django.urls import include, path
 from django.contrib import admin
 
-# Register your models here.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('users.urls')),   # routes requests to your users app
+    path('blog/', include('blog.urls')),   # routes requests to your blog app
+]
