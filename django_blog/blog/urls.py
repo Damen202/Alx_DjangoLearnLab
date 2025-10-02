01189d4e5fb93d765a36569b107db5aeaafd703c
Damen202 <babatundeogundare195@gmail.com>
2025-10-02 23:15:21 +0100

    Comments

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,29 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+from blog.views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
+
+
+urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
+
+    # Blog post routes
+    path('post/', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
+
+from blog import views
 
 urlpatterns = [
     # Post URLs
@@ -13,4 +37,6 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+
 ]
+
